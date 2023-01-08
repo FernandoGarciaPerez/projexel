57273132882dbfb0dfe36c6fc867e48c44d4ec19
Fernando Garcia Perez <fernando03gar729@gmail.com>
2023-01-08 07:37:34 -0600

    uno

---
+++ static/js/index.js
@@ -0,0 +1,88 @@
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
+
+
+
+//Data science
+function add1() {
+    document.getElementById("infoserv").innerHTML = 
+      "<h2>Machine Learning</h2> <p>Fundamentally, machine learning (ML) uses specialized algorithms to collect "+
+      "and evaluate input data to predict products whose values are within acceptable ranges. As new data is placed "+
+      "into these ML algorithms, they begin to understand and improve their processes to advance the performance, evolving their ‘intelligence’ over time.</p> "+
+      " <p>At Projexel, we understand the difficulties our clients may face, and we can apply our knowledge to help you turn those challenges into opportunities."+
+      " Our team has expertise in every potential application of machine learning to solve your most complex business problems.</p>";
+}
+
+function add2() {
+    document.getElementById("infoserv").innerHTML = 
+      "<h2> NLP </h2>  <p>Natural language processing (NLP) ultimately falls under the wide-ranging category of AI, but also shares essential characteristics "+
+      "of computational linguistics and computer science. Basically, NLP is the process by which machines learn to make sense of human language. Therefore, NLP"+
+      " results in a model that can successfully process text and remove any vagueness within it, a process is known as word sense disambiguation.</p> <p>For achieving"+
+      " the above-mentioned objective, machines need to be trained with textual data. For the purpose of training the machine, first, the textual training data is pre-processed "+
+      "in different ways based on the end objective. The rudimentary preprocessing techniques that are often used are tokenization, stemming, lemmatization, and stop word removal. "+
+      "These techniques which are considered baseline are used in some basic applications like document similarity and topic modeling among others. There are other techniques such "+
+      "as parts-of-speech tagging, named entity recognition that is used in combination with the previously mentioned baseline techniques for more complex applications like document "+
+      "summarization, sentiment analysis, and machine translation.</p> <p>In the current tech-fostered world, Projexel provides a fast and user-friendly service, and a good customer"+
+      " experience is a basic benchmark for a business or organization to succeed. Natural Language Processing enables us to provide that in the best of ways. Intelligent Virtual Assistants "+
+      "help us assist customers in resolving their issues with no human interference. Machine Translation systems help us get past the language barriers and extend our services to diverse "+
+      "communities. Sentiment Analysis will help us sense the mood of the customers towards our services from their reviews or feedback. </p> <span>These are just a few use-case scenarios "+
+      "among many others, and we will always have the flexibility to use NLP to cater to our customized and specific needs.</span>";
+}
+function add3() {
+    document.getElementById("infoserv").innerHTML = 
+      "<h2>Artificial Intelligence Consulting And Solutions</h2> <p>Projexel has more than 20 years of expertise in building APIs and software applications with GUIs "+
+      "that leverage artificial intelligence to learn and automate a myriad set of complex daily workflows. We build & configure software products and services to train "+
+      "intelligent recommendation and forecasting systems using state-of-the-art machine learning techniques. We are able to support the requirements set by the customer "+
+      "for image, video & speech processing platforms that enable users to leverage advanced computer vision and natural language processing techniques. </p>";
+}
+function add4() {
+    document.getElementById("infoserv").innerHTML = 
+      "<h2>Computer vision</h2> <p>Computer vision (CV) is the part of artificial intelligence (AI) that allows computers and "+
+      "systems to develop information from visual media such as digital images and videos and derive appropriate actions or programs "+
+      "based on the information gathered. Thus, artificial intelligence enables computers to consider appropriate actions, while CV enables "+
+      "computers to perceive and identify an individual's visual senses through vision. </p> <p>By utilizing a variety of algorithmic models to find "+
+      "out the perspective of the visual media that it is consuming. Over time, when a sufficient amount of data is processed through an algorithmic model, "+
+      "the computer begins to discern visual data. The entire process is completely independent of personal intervention during media evaluation. </p>" ;
+}
+
+
+
+//cloud services
+function add5() {
+    document.getElementById("infoserv").innerHTML = 
+      "<h2>Cloud Computing Solution and Services</h2> <p>With numerous cloud computing delivery models to choose from (private cloud, public cloud, hybrid models), organizations are struggling to settle on the most suitable option that addresses their existing challenges with their on-premise systems. At Projexel, we serve as your advisor in evaluating the many cloud computing options and help you understand the best course of action for implementation. Our team of advisors is here to support you in actualizing your investment in new technology, whether you have a lean IT staff and need augmented support to successfully deploy or you have a cloud first initiative that requires a complete modernization effort. Our holistic cloud approach results in the ability to scale up to evolving business needs and mitigate common issues that oftentimes accompany a cloud deployment. Our goal is to ensure a smooth and seamless transition to a cloud infrastructure with optimized uptimes and streamlined operational costs. </p>";
+}
+function add6() {
+    document.getElementById("infoserv").innerHTML = 
+      "<h2>Cloud Storage Solutions & Services</h2> <p>When it comes to Cloud Storage solutions, there are a vast amount of options suitable for every tier from a GB to PB and beyond! Oftentimes, cloud storage providers are priced very competitively, pennies to the GB, and offer similar micro services (object storage, cold storage, block storage, etc.). However, each varying provider does offer niche capabilities and solution differentiators, such as enhanced encryption and security or policies that support incremental backups. At Projexel, we work with our Customers to first understand their storage needs and requirements. We help to identify leading cloud storage solutions and providers and facilitate demos and evaluation criteria for downselect. Our cloud architects are integral to developing a comprehensive plan and implementing the appropriate storage solution. </p>";
+}
+function add7() {
+    document.getElementById("infoserv").innerHTML = 
+      "<h2>Cloud Infrastructure</h2> <p>Many organizations and agencies alike are faced with the challenge of modernizing their entire IT stack, including their data center infrastructure. End of life hardware and software within a data center requires an organization to evaluate and assess the benefits and feasibility of moving to a cloud-based infrastructure versus a costly hardware refresh. At Projexel, we bring value to organizations looking to modernize their infrastructure whether it be on-premise, public or private cloud. Our experts can provide support and expertise across security, leading Infrastructure as a Services (IaaS) cloud solution providers (CSPs), and best practices to ensure continuity of operations throughout implementation, migration, and ongoing systems administration and operations and maintenance (O&M). </p>";
+}
+function add8() {
+    document.getElementById("infoserv").innerHTML = 
+      "<h2>Cloud Backup & Disaster Recovery</h2> <p>Protecting our data is mission critical, and planning for We are helping organizations with implementing backup solutions and Disaster Recovery (DR) and Continuity of Operations (COOP) planning. Oftentimes, organizations are legally required to archive data and retain it for a certain period of time. The challenge is finding a feasible backup and archive solution that integrates with an entire organization’s data sources and IT stack. Projexel can support your organization in developing your backup and DR strategy, implementing backup solutions and configuring policies to ensure compliance, as well as provide augmented ongoing support services for existing DR & COOP policies. </p>";
+}
+function add9() {
+    document.getElementById("infoserv").innerHTML = 
+      "<h2>Cloud PBX</h2> <p>As VoIP and Unified Communication as a Service (UCaaS) solutions emerge, Projexel is offering Cloud PBX services to manage our Customer’s phone systems. We bring expertise in Cloud PBX solutions, including cloud hosted phone systems, security, Quality of Service (QoS), and VoIP protocols (i.e., SIP, MGCP, RTP, RTCP, SRTP, and XMPP). We support our Customers in understanding Cloud PBX and Cloud VoIP solutions in the marketplace and the viability and feasibility of potential solutions. By leveraging VoIP technology, organizations can increase bandwidth efficiency and reduce costs, as both voice and data communications run over a single network reducing infrastructure costs. </p>";
+}
+function add10() {
+    document.getElementById("infoserv").innerHTML = 
+      "<h2>Cloud App Dev Solutions</h2> <p>With the takeover of mobile devices and smartphones, we have become accustomed to an “App” society from our personal lives to our professional lives. This has created a saturation of app dev shops over the past several years. At Projexel, we are here to support your app dev needs. We bring capabilities and qualifications in custom app dev projects and leverage an agile development methodology approach and iterative SCRUM framework for rapid and complex development sprints. Our team of developers bring expertise across all phases of an app dev project - Assess, Design, Develop, Test, Deploy, and Ongoing Support & Continuous Improvement. </p>";
+}
+
+
+//Managed services
+
+function add11() {
+    document.getElementById("infoserv").innerHTML = 
+      " ";
+}
+
+function deleteinf() {
+    document.getElementById("infoserv").innerHTML = 
+      " ";
+}
